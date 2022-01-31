202fe8f7b56049f04f0e498ca96412a2445005ba
MAURICIO BELLO <bello900@hotmail.com>
2022-01-30 21:13:53 -0500

    OK

--- MongoDbApp/ModelDbConexion/MongoDBRepository.cs
+++ MongoDbApp/ModelDbConexion/MongoDBRepository.cs
@@ -18,11 +18,11 @@ namespace MongoDbApp.Models.ModelDbConexion
             //client = new MongoClient("mongodb://127.0.0.1:27001,127.0.0.1:27002,127.0.0.1:27003/?safe=true&connect=replicaset");
             //client = new MongoClient("mongodb://127.0.0.1:27001,127.0.0.1:27002,127.0.0.1:27003/?replicaSet=replicaIBERO");
             //client = new MongoClient("mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb");
-
             var settings = new MongoClientSettings
             {
                 Servers = new[]
                 {
+                    new MongoServerAddress("localhost", 27017),
                     //new MongoServerAddress("localhost", 26001),
                     //new MongoServerAddress("localhost", 26002),
                     //new MongoServerAddress("localhost", 26003),
@@ -30,25 +30,23 @@ namespace MongoDbApp.Models.ModelDbConexion
                     //new MongoServerAddress("localhost", 27006),
                     //new MongoServerAddress("localhost", 27007),
                     //new MongoServerAddress("localhost", 27008),
-                    new MongoServerAddress("localhost", 27009),
-                    new MongoServerAddress("localhost", 27010),
-                    new MongoServerAddress("localhost", 27011)
+                    //new MongoServerAddress("localhost", 27009),
+                    //new MongoServerAddress("localhost", 27010),
+                    //new MongoServerAddress("localhost", 27011)
                 },
                 //ApplicationName = "appTorneoDeportivo",
-                ConnectionMode = ConnectionMode.ReplicaSet,
-                //ConnectionMode = ConnectionMode.Automatic,
+                //ConnectionMode = ConnectionMode.ReplicaSet,
+                ConnectionMode = ConnectionMode.Automatic,
                 ReadPreference = ReadPreference.Secondary,
                 //DirectConnection=false,
                 //ReplicaSetName = "replicaTorneoIB",
-                ReplicaSetName = "replicaTorneoP2",
                 //ReplicaSetName = "replicaTorneoP2",
-                //ReplicaSetName = "replicaTorneoShardServer",
                 //ReplicaSetName = "replicaTorneoP2",
                 WriteConcern = new WriteConcern(WriteConcern.WValue.Parse("3"), wTimeout: TimeSpan.Parse("10"))
             };
             client = new MongoClient(settings);
 
-            db = client.GetDatabase("TorneoDeportivoIBERO");
+            db = client.GetDatabase("TorneoDeportivo2");
         }
     }
 }
